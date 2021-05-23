82068b541008fe9c7a7a5dff056767da45dd93a4
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2021-05-22 19:33:52 -0500

    Inicio de react e inicio de la navbar

---
+++ src/components/navigation/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// SCSS
+import './NavBar.scss';
+
+export default function NavBar() {
+    return (
+        <div className="navbar">
+            
+        </div>
+    )
+}
