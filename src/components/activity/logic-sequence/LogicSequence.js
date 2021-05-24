6293588a186ce203c014f7c538832d952b6f7241
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-05-24 18:36:42 -0500

    Now the sequence cards can be dragged and dropped around the list for sort them

--- src/components/activity/logic-sequence/LogicSequence.js
+++ src/components/activity/logic-sequence/LogicSequence.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 import './LogicSequence.scss';
 
@@ -7,33 +7,83 @@ import api from '../../../services/api';
 
 import SequenceCard from './SequenceCard';
 import CardDataPanel from './CardDataPanel';
-import { useDrop } from 'react-dnd';
-import { Itemtypes } from '../../../util/item';
+
+export const LogicSequenceContext = createContext({
+    sortList: null
+})
 
 const LogicSequence = props => {
 
-    const [{isOver}, drop] = useDrop({
-        accept: Itemtypes.SEQUENCE_CARD,
-        drop: (item, monitor) => console.log("something has been dropped!"),
-        collect: monitor => ({
-            isOver: !!monitor.isOver()
-        })
-    })
+    const [sequenceList, setSequenceList] = useState([
+        {
+            id:"1",
+            name:"This is my first sequence"
+        },
+        {
+            id:"2",
+            name:"This is my second sequence"
+        },
+        {
+            id:"3",
+            name:"This is my third sequence"
+        },
+        {
+            id:"4",
+            name:"This is my fourth sequence"
+        }
+    ])
+
+    const sortList = (dragged_id, target_id) => {
+        console.log("dragged_id")
+        console.log(dragged_id)
+        console.log("target_id")
+        console.log(target_id)
+      
+        if(dragged_id!== target_id){
+         
+            
+            const dragged = sequenceList.filter((sequence, i) => sequence.id === dragged_id)
+            const draggedPosition=sequenceList.map((i) => {return i.id;}).indexOf(dragged_id);
+            const targetPosition=sequenceList.map((i) => {return i.id;}).indexOf(target_id);
+            sequenceList.splice(draggedPosition, 1)
+            sequenceList.splice(targetPosition, 0, dragged[0]);
+            console.log("dragged")
+            console.log(dragged)
+            console.log("targetPosition")
+            console.log(targetPosition)
+            console.log("sequenceList")
+            console.log(sequenceList)
+
+            console.log("last")
+            const lastItem = sequenceList[sequenceList.length - 1]
+            console.log(lastItem)
+            sequenceList.splice(sequenceList.length - 1, 1)
+            console.log("sequenceList after")
+            console.log(sequenceList)
+
+            
+            setSequenceList(sequenceList.concat(lastItem))
+        }
+        
+
+    };
 
     return (
-        <div className="logic-sequence-container">
-            <h1>Name of the logic sequence</h1>
-            <p>Desciption of the logic sequence activity</p>
-            <hr></hr>
-            <div className="sequence-cards-container" ref={drop}>
-                <SequenceCard></SequenceCard>
-                <SequenceCard></SequenceCard>
-                <SequenceCard></SequenceCard>
-                <SequenceCard></SequenceCard>
+        <LogicSequenceContext.Provider value={{sortList}}>
+            <div className="logic-sequence-container">
+                <h1>Name of the logic sequence</h1>
+                <p>Desciption of the logic sequence activity</p>
+                <hr></hr>
+                <div className="sequence-cards-container">
+                    {sequenceList
+                        .map((sequence, i) => (
+                            <SequenceCard sequenceName={sequence.name} sequenceId={sequence.id}></SequenceCard>
+                        ))}
+                </div>
+                <CardDataPanel>
+                </CardDataPanel>
             </div>
-            <CardDataPanel>
-            </CardDataPanel>
-        </div>
+        </LogicSequenceContext.Provider>
     )
 }
 
--- src/components/activity/logic-sequence/SequenceCard.js
+++ src/components/activity/logic-sequence/SequenceCard.js
@@ -1,25 +1,43 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import './SequenceCard.scss';
 
 // to make API calls
 import api from '../../../services/api';
-import { useDrag } from 'react-dnd';
+import { useDrag, useDrop } from 'react-dnd';
 import { Itemtypes } from '../../../util/item';
+import { LogicSequenceContext } from './LogicSequence';
 
 const SequenceCard = props => {
 
-    const [{isDragging}, drag, opacity] = useDrag({
+    const { sortList } = useContext(LogicSequenceContext);
+
+    const [{isDragging}, drag] = useDrag({
         type: Itemtypes.SEQUENCE_CARD,
-        item: 'asd',
+        item: {id:props.sequenceId},
         collect: monitor => ({
             isDragging: !!monitor.isDragging(),
         }),
     })
 
+    const [{isOver}, drop] = useDrop({
+        accept: Itemtypes.SEQUENCE_CARD,
+        drop: (item, monitor) => {
+            console.log("something has been dropped!");
+            sortList(item.id, props.sequenceId);
+        },
+        collect: monitor => ({
+            isOver: !!monitor.isOver()
+        })
+    })
+
     return (
-        <div className="sequence-card-container" ref={drag} style={{opacity:isDragging?"0.5":"1"}}>
-            <h1>Card name</h1>
+        <div className="sequence-card-container" ref={drop} style={{
+            backgroundColor: isOver?"red":"white",
+          }}>
+            <div className="secundary-sequence-card-container" ref={drag} style={{opacity:isDragging?"0.5":"1"}}>
+                <h1>{props.sequenceName}</h1>
+            </div>
         </div>
     )
 }
