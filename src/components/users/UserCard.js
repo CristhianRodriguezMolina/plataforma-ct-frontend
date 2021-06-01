3390fec7dfd58bff63c7520d669f99014b7fb0d8
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2021-06-01 11:37:40 -0500

    synch

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -78,6 +78,8 @@ export default function UserCard({ user, setUsers, history, type }) {
         }
     }
 
+    console.log(typeof user.birth_date)
+
     return (
         <div className="user-card d-flex justify-content-between mb-4">
             <div className="card d-flex justify-content-center align-items-left mr-3">
@@ -85,7 +87,7 @@ export default function UserCard({ user, setUsers, history, type }) {
                     <Avatar className="mr-2" src="https://i.pinimg.com/originals/32/a3/69/32a3690fe66a73adcb98922874eb8b8a.jpg"/>
                     <div className="ml-2 mr-5">
                         <p className="m-0">{user.first_name} {user.last_name}</p>                            
-                        <p className="m-0 text-muted">Edad: {user.age}</p>                            
+                        <p className="m-0 text-muted">Edad: {Math.floor((new Date().getTime() - new Date(user.birth_date).getTime())/(1000 * 3600 * 24)/365)} años</p>                            
                         <p className="m-0 text-muted">ID: {user.id}</p>                            
                     </div>
                     {success?
@@ -104,7 +106,7 @@ export default function UserCard({ user, setUsers, history, type }) {
             </div>
             <div className="d-flex flex-column">
                 <div className="button-group btn-group-vertical">
-                    <Link to={`/user/${type}/edit/${user._id}`} className="btn btn-primary align-items-center" data-toggle="modal" data-target="#userDetail">Editar</Link>                    
+                    <Link to={`/user/${type}/edit/${user._id}`} className="btn btn-primary d-flex justify-content-center align-items-center" data-toggle="modal" data-target="#userDetail">Editar</Link>                    
                     <button onClick={deleteUser} className="btn btn-danger" data-toggle="modal" data-target="#deleteUser">Borrar</button>                    
                 </div>
             </div>
