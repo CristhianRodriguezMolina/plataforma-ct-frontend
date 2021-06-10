b9524f47fe12c2e3922ef1191908cfd2588d3e22
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-06-09 19:05:06 -0500

    Now you can update units (name, description and visibility)

--- src/components/course/teacher-view/UnitContent.js
+++ src/components/course/teacher-view/UnitContent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './UnitContent.scss';
 
@@ -9,70 +9,100 @@ import TaskCard from '../task/TaskCard';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
+import PropTypes from 'prop-types';
+
 const UnitContent = props => {
 
-    const [unitName, setUnitName] = useState("Unidad 1");
-    const [unitDes, setUnitDes] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus nulla, lobortis a interdum et, fringilla eu nunc. Praesent imperdiet orci malesuada nunc egestas, vel hendrerit ante ultricies. In hac habitasse platea dictumst. Nulla consectetur purus ut neque ultrices, id dignissim quam fringilla. Duis elementum ultricies velit sit amet ultrices. ");
-    const [visible, setVisible] = useState(false);
+	const [unitName, setUnitName] = useState("");
+	const [unitDes, setUnitDes] = useState("");
+	const [visible, setVisible] = useState(false);
 
-    const updateName = (value) => {
-        setUnitName(value);
-    };
+	const updateName = (value) => {
+		setUnitName(value);
+	};
 
-    const updateDes = (value) => {
-        setUnitDes(value);
-    };
+	const updateDes = (value) => {
+		setUnitDes(value);
+	};
 
-    const nameInputStyle = {
-        width: "100%",
-        fontSize: "1.7em",
-        margin: "0",
-        padding: "0.4em",
-        lineHeight: "1.2em",
-        fontWeight: "600"
-    };
+	useEffect(() => {
+		if (props.unitValue) {
+			setUnitName(props.unitValue.name);
+			setUnitDes(props.unitValue.description);
+			setVisible(props.unitValue.visible);
+		}
+	}, [props.unitValue]);
 
-    const desInputStyle = {
-        width: "100%",
-        fontSize: "0.8em",
-        margin: "0",
-        padding: "0.7em",
-        overflow: "hidden",
-        lineHeight: "1.2em",
-        fontWeight: "500",
-        minHeight: "2.5em"
-    };
+	const nameInputStyle = {
+		width: "100%",
+		fontSize: "1.7em",
+		margin: "0",
+		padding: "0.4em 0.3em",
+		lineHeight: "1.2em",
+		fontWeight: "600"
+	};
 
-    const handleChange = () => {
-        setVisible(!visible);
-       console.log('Switched!');
-    };
+	const desInputStyle = {
+		width: "100%",
+		fontSize: "0.8em",
+		margin: "0",
+		padding: "0.7em",
+		overflow: "hidden",
+		lineHeight: "1.2em",
+		fontWeight: "500",
+		minHeight: "2.5em"
+	};
 
-    return (
-        <div className="unit-content-container">
-            <div className="logic-sequence-info">
-                <DynamicInput dynamicInputValue={unitName} dynamicInputStyle={nameInputStyle} sendValue={updateName}></DynamicInput>
-                <DynamicInput dynamicInputValue={unitDes} dynamicInputStyle={desInputStyle} sendValue={updateDes}></DynamicInput>
-            </div>
-            <div className="buttons-container">
-                <button type="submit" className="btn btn-info m-2">Guardar cambios</button>
-                <FormControlLabel className="switcher" label="Visible" control= {
-                    <Switch
-                        checked={visible}
-                        onChange={handleChange}
-                        name="checkedB"
-                        color="primary"
-                    />
-                }/>
-            </div>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-            <TaskCard/>
-        </div>
-    )
+	const handleChange = () => {
+		setVisible(!visible);
+	};
+
+	const handleUpdateChanges = () => {
+		props.onUpdateChanges({
+			_id: props.unitValue._id,
+			visible: visible,
+			name: unitName,
+			description: unitDes,
+			tasks: props.unitValue.tasks
+		});
+	};
+
+	return (
+		<div className="unit-content-container">
+			<div className="unit-content-info">
+				<DynamicInput dynamicInputValue={unitName} dynamicInputStyle={nameInputStyle} sendValue={updateName}></DynamicInput>
+				<DynamicInput dynamicInputValue={unitDes} dynamicInputStyle={desInputStyle} sendValue={updateDes}></DynamicInput>
+			</div>
+			<div className="buttons-container">
+				<button type="submit" onClick={handleUpdateChanges} className="btn btn-info m-2">Guardar cambios</button>
+				<FormControlLabel className="switcher" label="Visible" control={
+					<Switch
+						checked={visible}
+						onChange={handleChange}
+						name="visibilty"
+						color="primary"
+					/>
+				} />
+			</div>
+			<div className="cards-container">
+				<TaskCard />
+				<TaskCard />
+				<TaskCard />
+				<TaskCard />
+				<TaskCard />
+				<TaskCard />
+			</div>
+		</div>
+	)
 };
+
+UnitContent.propTypes = {
+	unitValue: PropTypes.shape({
+		name: PropTypes.string,
+		description: PropTypes.string,
+		visible: PropTypes.bool
+	}),
+	onUpdateChanges: PropTypes.func
+}
 
 export default UnitContent;
