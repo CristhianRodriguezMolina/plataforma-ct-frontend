7f5611cbcc275476c1e8b1d15371cec778aacdaf
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-06-09 19:21:33 -0500

    DynamicInput Optimized

--- src/components/common/DynamicInput.js
+++ src/components/common/DynamicInput.js
@@ -6,65 +6,71 @@ import './DynamicInput.scss';
 
 const DynamicInput = props => {
 
-    const [value, setValue] = useState(null);
-    const [showInput, setShowInput] = useState(false);
-    const [height, setHeight] = useState(0); 
+	const [value, setValue] = useState(null);
+	const [showInput, setShowInput] = useState(false);
+	const [height, setHeight] = useState(0);
 
-    const h1 = useRef(null);
-    const input = useRef(null);
-    
-    useEffect(() => {
-        // if(!value) {
-            setValue(props.dynamicInputValue);
-        // }
-    }, [props.dynamicInputValue]);
+	const h1 = useRef(null);
+	const input = useRef(null);
 
-    useEffect(() => {
-        if(showInput) {
-            input.current.focus();
-            input.current.selectionStart = input.current.value.length;
-            input.current.selectionEnd = input.current.value.length;
-        }
-    }, [showInput]);
+	useEffect(() => {
+		// if(!value) {
+		setValue(props.dynamicInputValue);
+		// }
+	}, [props.dynamicInputValue]);
 
-    const handleInputClick = () => {
-        setHeight(h1.current.clientHeight);
-        setShowInput(true);
-    }
+	useEffect(() => {
+		if (showInput) {
+			input.current.focus();
+			input.current.selectionStart = input.current.value.length;
+			input.current.selectionEnd = input.current.value.length;
+		}
+	}, [showInput]);
 
-    const handleKeyDownInput = (event) => {
-        if (event.key === 'Enter') {
-            setShowInput(false);
-        }
+	const handleInputClick = () => {
+		setHeight(h1.current.clientHeight);
+		setShowInput(true);
+	}
 
-        event.target.style.height = 'inherit';
-        event.target.style.height = `${event.target.scrollHeight}px`;
-    }
-   
-    const newStyle = {...props.dynamicInputStyle, height:height};
+	const handleKeyDownInput = (event) => {
+		if (event.key === 'Enter') {
+			setShowInput(false);
+			props.sendValue(event.target.value);
+		}
 
-    const onChangeHandler = (evt) => {
-        setValue(evt.target.value);
-        props.sendValue(evt.target.value);
-    }
-    
-    return (
-        <div className="dynamic-input-container">
-            
-            {showInput? 
-            <textarea type="text" rows={1} style={newStyle} ref={input} onKeyDown={handleKeyDownInput} 
-                className="form-control dynamic-input" value={value} onBlur={() => setShowInput(false)} 
-                onChange={onChangeHandler}>
-            </textarea>
-            : <h1 style={props.dynamicInputStyle} ref={h1} onClick={handleInputClick} className="dynamic-label">{value}</h1> }
-        </div>
-    )
+		event.target.style.height = 'inherit';
+		event.target.style.height = `${event.target.scrollHeight}px`;
+	}
+
+	const newStyle = { ...props.dynamicInputStyle, height: height };
+
+	const onChangeHandler = (evt) => {
+		setValue(evt.target.value);
+		// props.sendValue(evt.target.value);
+	}
+
+	const handleOnblur = async (evt) => {
+		setShowInput(false);
+		props.sendValue(evt.target.value);
+	};
+
+	return (
+		<div className="dynamic-input-container">
+
+			{showInput ?
+				<textarea type="text" rows={1} style={newStyle} ref={input} onKeyDown={handleKeyDownInput}
+					className="form-control dynamic-input" value={value} onBlur={handleOnblur}
+					onChange={onChangeHandler}>
+				</textarea>
+				: <h1 style={props.dynamicInputStyle} ref={h1} onClick={handleInputClick} className="dynamic-label">{value}</h1>}
+		</div>
+	)
 };
 
 DynamicInput.propTypes = {
-    dynamicInputValue: PropTypes.string,
-    dynamicInputStyle: PropTypes.object, 
-    sendValue: PropTypes.func.isRequired
+	dynamicInputValue: PropTypes.string,
+	dynamicInputStyle: PropTypes.object,
+	sendValue: PropTypes.func.isRequired
 }
 
 export default DynamicInput;
